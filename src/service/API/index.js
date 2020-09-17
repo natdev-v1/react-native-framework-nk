e57cf07dc690e4add51b76a628a14a374b8ed302
Test101Bug <zookeroro@gmail.com>
2020-09-17 10:42:27 +0700

    add comment

--- src/service/API/index.js
+++ src/service/API/index.js
@@ -3,8 +3,11 @@ import AsyncStorage from '@react-native-community/async-storage';
 import ApiLogin from './ApiRegister/ApiLogin';
 
 const APIService = async () => {
+  // get Tokey from local store
   const token = await AsyncStorage.getItem('token');
+  // for create http when Project have other service
   let http = createApi('', {Authorization: token ? token : ''});
+  // Mapping All Api to Object
   return {
     ...ApiLogin(http),
   };
